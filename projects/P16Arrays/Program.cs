ec7e770b3a2a9f3753372c76a196c75dd1fe5c31
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-01-31 16:17:45 +0100

    P16Arrays assignment

---
+++ projects/P16Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿Random random = new Random();
+int[] array = new int[11] {0,1,2,3,4,5,6,7,8,9,10};
+
+Console.WriteLine("I will roll 10.000 numbers between 0 and 10");
+for (int roll = 0; roll <= 10000; roll++)
+{
+    int randomNum = random.Next(0, array.Length);
+    array[randomNum]++;
+
+}
+
+for (int index = 0; index <= 10; index++) 
+{
+    Console.WriteLine($"I rolled {index} a total of {array[index]} times");
+}
