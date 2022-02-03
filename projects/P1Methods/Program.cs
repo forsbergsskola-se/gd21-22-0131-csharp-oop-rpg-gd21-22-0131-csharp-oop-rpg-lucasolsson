bca83ed8a40950cb945930588f1c9f87116893d2
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-03 12:49:40 +0100

    Updated P1Methods with Parameters 2.1

--- projects/P1Methods/Program.cs
+++ projects/P1Methods/Program.cs
@@ -3,35 +3,36 @@ Main();
 
 static void Main()
 {
-    for (int i = 0; i <= 1; i++)
-    {
-        CountUp();
-        CountDown();
-    }
+    CountUp(0,1);
+    CountDown(1,0);
+    CountUp(0,2);
+    CountDown(2,0);
+    CountUp(0,3);
+    CountDown(3,0);
 }
 
-static void CountUp() //CountUp method
+static void CountUp(int from, int to) //CountUp method
 {
-    Console.WriteLine("Counting from 0 to 5:");
-    for (int counter = 0; counter <= 5; counter++) // condition-statement
+    Console.WriteLine($"Counting from {from} to {to}:");
+    for (int counter = from; counter <= to; counter++) // condition-statement
     {
         Console.WriteLine(counter); // loop-body
     }
 }
-// initial-statement = int counter = 0;
-// condition-statement = counter <= 5; 
+// initial-statement = int counter = x number;
+// condition-statement = counter <= x; 
 // loop-body = Console.WriteLine(counter); (write out the counter number until int counter is equal to 5)
 // iteration-statement = counter++ (make sure the counter continue of ticking up)
 
-static void CountDown() //CountDown method
+static void CountDown(int from, int to) //CountDown method
 {
-    Console.WriteLine("Counting from 5 to 0:");
-    for (int counter = 5; counter >= 0; counter--)
+    Console.WriteLine($"Counting from {from} to {to}:");
+    for (int counter = from; counter >= to; counter--)
     {
         Console.WriteLine(counter); 
     }
 }
-// initial-statement = int counter = 5;
-// condition-statement = counter >= 0; 
+// initial-statement = int counter = x number;
+// condition-statement = counter >= x; 
 // loop-body = Console.WriteLine(counter); (write out the counter number until int counter is equal to 0)
 // iteration-statement = counter-- (make sure the counter continue of ticking down)
