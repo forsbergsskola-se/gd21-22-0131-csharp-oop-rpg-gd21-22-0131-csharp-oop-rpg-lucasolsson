af986bc4b50e14e9130487c1386896123218bcbd
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-03 13:44:18 +0100

    Added comments to P1Methods

--- projects/P1Methods/Program.cs
+++ projects/P1Methods/Program.cs
@@ -1,4 +1,4 @@
-﻿//Call the method
+﻿//Call the Main method
 Main();
 
 static void Main()
@@ -12,20 +12,27 @@ static void Main()
 
 static void Count(int from, int to) //Count method
 {
-    if (from < to)
+    if (from < to) //Do this if the number "from" smaller than number "to"
     {
         Console.WriteLine($"Counting from {from} to {to}:");
         for (int counter = from; counter <= to; counter++)
          {
+             //loop-body
             Console.WriteLine(counter);
          }
     }
-    else
+    else //Do this if the number "to" smaller than number "from"
     {
         Console.WriteLine($"Counting from {from} to {to}:");
         for (int counter = from; counter >= to; counter--)
         {
+            //loop-body
             Console.WriteLine(counter); 
         }
     }
 }
+
+//initial-statement = int counter = from;
+// condition-statement = counter <= or counter >=
+//loop-body = Do the things inside the loop, in this case write out int "counter"
+// iteration-statement = Do things before checking the condition statement again, in this case if counter is smaller/bigger or equal to int "to". 
