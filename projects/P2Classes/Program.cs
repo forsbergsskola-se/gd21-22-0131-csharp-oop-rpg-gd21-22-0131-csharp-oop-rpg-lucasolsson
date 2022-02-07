71bc85c580c90fadea9d77b706aabaeef3739db2
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-07 14:28:49 +0100

    P2Classes first part

---
+++ projects/P2Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Person myPerson = new Person();
+Animal myAnimal = new Animal();
+Car myCar = new Car();
+
+myPerson.person();
+myAnimal.animal();
+myCar.car();
+
+public class Person
+{
+    public void person()
+    {
+        Console.WriteLine("Person");
+    }
+}
+public class Animal
+{
+    public void animal()
+    {
+        Console.WriteLine("Animal");
+    }
+}
+public class Car
+{
+    public void car()
+    {
+        Console.WriteLine("Car");
+    }
+}
