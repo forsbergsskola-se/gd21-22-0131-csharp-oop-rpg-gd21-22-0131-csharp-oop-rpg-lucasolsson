64edc38fd46ff1374e1f1e7c4109ad4def1deed0
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-07 16:15:21 +0100

    P2Classes part 2 of the assignment

--- projects/P2Classes/Program.cs
+++ projects/P2Classes/Program.cs
@@ -1,18 +1,37 @@
 ﻿
-Person myPerson = new Person();
-Animal myAnimal = new Animal();
-Car myCar = new Car();
+// Person myPerson = new Person();
+// Animal myAnimal = new Animal();
+// Car myCar = new Car();
+//
+// // This calls the different-methods in the classes on the instance
+// myPerson.person();
+// myAnimal.animal();
+// myCar.car();
 
-myPerson.person();
-myAnimal.animal();
-myCar.car();
+//Calls out the method in class Person
+Person person = new Person();
+person.IntroduceYourself();
 
 public class Person
 {
-    public void person()
+    public string name;
+    string[] persons = new string[3];
+  
+    public void IntroduceYourself() //Instance method
     {
-        Console.WriteLine("Person");
+        for(var name = 0; name < persons.Length; name++)
+        { 
+            //Prints and wants input 3 times and stores the input in array "persons"
+            Console.WriteLine("Give me a name.");
+            persons[name] = Console.ReadLine();
+        }
+
+        foreach (string value in persons)
+        {
+            Console.WriteLine($"Hello, my name is {value}");
+        }
     }
+    
 }
 public class Animal
 {
