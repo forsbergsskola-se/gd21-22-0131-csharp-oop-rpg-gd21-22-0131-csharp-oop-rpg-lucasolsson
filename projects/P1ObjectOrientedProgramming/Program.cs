b21698e50a0b4e3476e89cfb42355e96fdbd5304
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-07 12:43:39 +0100

    P1ObjectOrientedProgramming assignment

---
+++ projects/P1ObjectOrientedProgramming/Program.cs
@@ -0,0 +1,15 @@
+﻿    string[] names = new string[3];
+
+    for (var i = 0; i < names.Length; i++)
+    { 
+         //Prints and wants input 3 times and stores the input in array "names"
+         Console.WriteLine("Give me a name.");
+         names[i] = Console.ReadLine();
+         
+    }
+    
+    foreach (string value in names) 
+    {
+         //prints out the value in the array after each other on the same line
+         Console.Write($"{value}, ");
+    }
