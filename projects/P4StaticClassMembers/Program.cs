c29332c4dea4ad3ebc45de7fa9c90a5a48d201b1
Spelkojan <lucas.olsson@student.forsbergsskola.se>
2022-02-15 15:42:50 +0100

    P4StaticClassMembers assignment

---
+++ projects/P4StaticClassMembers/Program.cs
@@ -0,0 +1,33 @@
+﻿
+static class SuperMath
+{
+    static void Main()
+    {
+        Console.WriteLine(Lerp(0, 100, 0));
+        Console.WriteLine(Lerp(0, 100, 0.1f));
+        Console.WriteLine(Lerp(0, 100, 0.3f));
+        Console.WriteLine(Lerp(0, 100, 0.7f));
+        Console.WriteLine(Lerp(0, 100, 1.0f));
+        Console.WriteLine(Lerp(0, 100, 1.3f));
+        Console.WriteLine(Lerp(100, 200, 0));
+        Console.WriteLine(Lerp(100, 200, 0.1f));
+        Console.WriteLine(Lerp(100, 200, 0.3f));
+        Console.WriteLine(Lerp(100, 200, 0.7f));
+        Console.WriteLine(Lerp(100, 200, 1.0f));
+        Console.WriteLine(Lerp(100, 200, 1.3f));
+        Console.WriteLine(Lerp(200, -100, 0));
+        Console.WriteLine(Lerp(200, -100, 0.1f));
+        Console.WriteLine(Lerp(200, -100, 0.3f));
+        Console.WriteLine(Lerp(200, -100, 0.7f));
+        Console.WriteLine(Lerp(200, -100, 1.0f));
+        Console.WriteLine(Lerp(200, -100, 1.3f));
+        
+    }
+    
+    static float Lerp(float from, float to, float t)
+    {
+        float answer = from + (to - from) * t;
+        Console.Write($"Lerp from {from} to {to} with t({t}):");
+        return answer;
+    }
+}
